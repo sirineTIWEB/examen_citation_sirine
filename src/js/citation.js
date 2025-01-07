8b161ccbc291018aa013432cad304eaf39ee9fdc
sirineTIWEB <sirine.elalami.22578@stu.he-ferrer.eu>
2025-01-07 11:23:25 +0100

    leboutonmarche

--- src/js/citation.js
+++ src/js/citation.js
@@ -23,14 +23,60 @@ $(document).ready(function() {
 
                 $('.auteur').html(monAuteur);
                 $('.citation').append(maCitation);
+
                 $('#RefreshButton').click(function() {
-                    data[Math.floor(Math.random() * data.length)];
+                    var monAuteur ="";
+                    var maCitation = "";
+                    $('.citation').empty();
+                    const randomItemButtoned = data[Math.floor(Math.random() * data.length)];
+
+                    monAuteur += "<img src='img/" + randomItemButtoned.Photo + "' alt='"+randomItemButtoned.Auteur+"' class='rounded-full h-16 aspect-square object-cover' /> <h2 class='mx-4 font-serif font-medium'>" + randomItemButtoned.Auteur + "</h2> <p class='border-l pl-4 italic border-blue-900'>" + randomItemButtoned.Description + "</p>";
+
+                    maCitation += "<div class='flex justify-between w-full'><h1 class='text-6xl font-serif'>''</h1><h1 class='text-6xl font-serif'>''</h1></div><h1 class='text-5xl font-serif'>" + randomItemButtoned.Citation + "</h1>";
+
+                    $('.auteur').html(monAuteur);
+                    $('.citation').append(maCitation);
                 });
             // });
         },
         error: function(xhr, status, error) {
             console.error('error:', error);
         }
+    });//end ajax
+
+    let deferredPrompt;
+    const installButton = document.getElementById('PWAbutton');
+    installButton.style.display = 'none';
+    const msgerreur = "Installation non disponible sur iOS";
+
+    // Détection si l'installation est possible
+    window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+
+    // Vérification de la plateforme
+    if (/iPhone|iPad|iPod/.test(navigator.platform)) {
+        installButton.html(msgerreur);
+        installButton.disabled = true;
+    } else {
+        installButton.style.display = 'block';
+    }
     });
+
+    // Gestion du clic sur le bouton d'installation
+    installButton.addEventListener('click', async () => {
+    if (!deferredPrompt) return;
+
+    const result = await deferredPrompt.prompt();
+    console.log(`Installation ${result.outcome}`);
+    deferredPrompt = null;
+    installButton.style.display = 'none';
+    });
+
+    // Détection si l'app est déjà installée
+    window.addEventListener('appinstalled', () => {
+    deferredPrompt = null;
+    installButton.style.display = 'none';
+});
     
 });
