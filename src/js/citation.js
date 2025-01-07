e51109cc3ba2b189ab8427f1234915c5671094e4
sirineTIWEB <sirine.elalami.22578@stu.he-ferrer.eu>
2025-01-07 10:54:19 +0100

    commit juste pour branche

--- src/js/citation.js
+++ src/js/citation.js
@@ -6,13 +6,30 @@ $(document).ready(function() {
         type: 'GET',
         dataType: 'json',
         success: function(data) {
-            $.each(data, function(index, item) {
-                console.log('hello');
-                console.log(data);
-            });
+            console.log('data:', data);
+
+            var monAuteur ="";
+            var maCitation = "";
+            var randomItem = data[Math.floor(Math.random() * data.length)];
+
+            // $.each(data, function(index, item) {
+                monAuteur += "<img src='img/" + randomItem.Photo + "' alt='"+randomItem.Auteur+"' class='rounded-full h-16 aspect-square object-cover' /> <h2 class='mx-4 font-serif font-medium'>" + randomItem.Auteur + "</h2> <p class='border-l pl-4 italic border-blue-900'>" + randomItem.Description + "</p>";
+
+                maCitation += "<h1 class='text-5xl font-serif'>" + randomItem.Citation + "</h1>";
+
+
+
+
+
+                $('.auteur').html(monAuteur);
+                $('.citation').append(maCitation);
+                $('#RefreshButton').click(function() {
+                    data[Math.floor(Math.random() * data.length)];
+                });
+            // });
         },
         error: function(xhr, status, error) {
-            console.error(error);
+            console.error('error:', error);
         }
     });
     
